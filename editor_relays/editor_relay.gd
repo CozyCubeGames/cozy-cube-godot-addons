632acbd9569dfb8597f1bcd180a34c0e990e8dac
Zi Ye <major.mcdoom@gmail.com>
2025-02-21 09:44:53 -0600

    Fixed some more bugs. Added a "active outside tree" option. Renamed "message group" to the more obvious "channel".

--- editor_relays/editor_relay.gd
+++ editor_relays/editor_relay.gd
@@ -7,17 +7,18 @@ signal message_received_from_game(message: StringName, data: Array)
 signal message_received_from_editor(message: StringName, data: Array)
 
 
-## All relays in the same message group will process the same messages. By default, all relays is under the same global group and will process all messages, so make sure to assign different group names if you want to isolate them.
-@export var message_group: StringName:
-	get: return message_group
+## All relays on the same channel will process the same messages. By default, all relays is under the same global channel and will process all messages, so make sure to assign different channel names if you want to isolate them.
+@export var channel: StringName:
+	get: return channel
 	set(value):
-		var old_message_group := message_group
-		message_group = value
+		var old_channel := channel
+		channel = value
 		if not Engine.is_editor_hint():
 			if is_node_ready():
-				EngineDebugger.unregister_message_capture("relay-" + old_message_group)
-				EngineDebugger.register_message_capture("relay-" + message_group, _on_message_captured)
-
+				EngineDebugger.unregister_message_capture("relay-" + old_channel)
+				EngineDebugger.register_message_capture("relay-" + channel, _on_message_captured)
+## If [code]true[/code], this relay remains active if removed from the scene tree.
+@export var active_outside_tree: bool
 
 var _plugin: EditorRelaysDebuggerPlugin
 
@@ -30,7 +31,7 @@ func _ready() -> void:
 			if is_instance_valid(_plugin):
 				_plugin._register_relay(self)
 	else:
-		EngineDebugger.register_message_capture("relay-" + message_group, _on_message_captured)
+		EngineDebugger.register_message_capture("relay-" + channel, _on_message_captured)
 
 
 func _notification(what: int) -> void:
@@ -41,7 +42,7 @@ func _notification(what: int) -> void:
 			if is_instance_valid(_plugin):
 				_plugin._unregister_relay(self)
 		else:
-			EngineDebugger.unregister_message_capture("relay-" + message_group)
+			EngineDebugger.unregister_message_capture("relay-" + channel)
 
 
 func send_message_to_game(message: StringName, data: Array = []) -> void:
@@ -52,7 +53,7 @@ func send_message_to_game(message: StringName, data: Array = []) -> void:
 	if is_instance_valid(_plugin):
 		var session := _plugin.get_session(0)
 		if is_instance_valid(session) and session.is_active():
-			session.send_message("relay-" + message_group + ":" + message, data)
+			session.send_message("relay-" + channel + ":" + message, data)
 
 
 func send_message_to_editor(message: StringName, data: Array = []) -> void:
@@ -60,12 +61,12 @@ func send_message_to_editor(message: StringName, data: Array = []) -> void:
 	if Engine.is_editor_hint():
 		return
 
-	EngineDebugger.send_message("relay-" + message_group + ":" + message, data)
+	EngineDebugger.send_message("relay-" + channel + ":" + message, data)
 
 
 func _on_message_captured(message: String, data: Array) -> bool:
 
-	if is_inside_tree():
+	if active_outside_tree or is_inside_tree():
 		message_received_from_editor.emit(message, data)
 		return true
 	return false
