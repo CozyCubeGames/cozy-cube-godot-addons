c8846abce488b82ee2254959f68a315671e8e23f
Zi Ye <major.mcdoom@gmail.com>
2025-06-02 08:59:00 -0500

    Changed TextEdit to a LineEdit in the New Gizmo Preset dialog.

--- gizmo_presets/new_gizmo_preset_dialog.gd
+++ gizmo_presets/new_gizmo_preset_dialog.gd
@@ -3,7 +3,7 @@ class_name NewGizmoPresetDialog
 extends ConfirmationDialog
 
 
-@export var preset_name_field: TextEdit
+@export var preset_name_field: LineEdit
 
 
 func _ready() -> void:
@@ -17,6 +17,6 @@ func get_preset_name() -> String:
 	return preset_name_field.text
 
 
-func _on_preset_name_field_text_changed() -> void:
+func _on_preset_name_field_text_changed(new_text: String) -> void:
 
-	get_ok_button().disabled = preset_name_field.text.strip_edges().is_empty()
+	get_ok_button().disabled = new_text.strip_edges().is_empty()
